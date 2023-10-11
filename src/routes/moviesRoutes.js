8c50de3e60a679949fecb15dd795cd5a10f80dbe
Lucas Arroyo <lucas.aa68@gmail.com>
2023-10-11 17:36:46 -0300

    add: Tp movies done!

---
+++ src/routes/moviesRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const { list, new: newest, recomended, delete: remove, create, add, detail, destroy, update, edit } = require('../controllers/moviesController');
+const {upload} = require('../middlewares/upload');
+router
+    .get('/movies', list)
+    .get('/movies/new', newest)
+    .get('/movies/recommended', recomended)
+    .get('/movies/detail/:id', detail)
+    //Rutas exigidas para la creación del CRUD
+    .get('/movies/add', add)
+    .post('/movies/create', upload.single('image'), create)
+    .get('/movies/edit/:id', edit)
+    .put('/movies/update/:id', upload.single('image'), update)
+    .get('/movies/delete/:id', remove)
+    .delete('/movies/delete/:id', destroy)
+
+module.exports = router;
